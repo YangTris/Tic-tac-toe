4f85880e6be2567675f0bb5bc3afd8eeeec6ad77
QuangHaii <hainoob2002@gmail.com>
2024-04-27 23:57:15 +0700

    Update win conditions

--- server.py
+++ server.py
@@ -55,18 +55,37 @@ def check_rows():
     # print("Checking rows")
     result = 0
     for i in range(9):
-        if matrix[i][0] == matrix[i][1] and matrix[i][1] == matrix[i][2]:
+        if matrix[i][0] == matrix[i][1] == matrix[i][2]:
             result = matrix[i][0]
             if result != 0:
                 break
+
+        if matrix[i][3] == matrix[i][4] == matrix[i][5]:
+            result = matrix[i][3]
+            if result != 0:
+                break
+        
+        if matrix[i][6] == matrix[i][7] == matrix[i][8]:
+            result = matrix[i][6]
+            if result != 0:
+                break
+
     return result
 
 def check_columns():
     # print("Checking cols")
     result = 0
     for i in range(9):
-        if matrix[0][i] == matrix[1][i] and matrix[1][i] == matrix[2][i]:
+        if matrix[0][i] == matrix[1][i] == matrix[2][i]:
             result = matrix[0][i]
+            if result != 0:
+                break
+        if matrix[3][i] == matrix[4][i] == matrix[5][i]:
+            result = matrix[3][i]
+            if result != 0:
+                break
+        if matrix[6][i] == matrix[7][i] == matrix[8][i]:
+            result = matrix[6][i]
             if result != 0:
                 break
     return result
@@ -74,10 +93,31 @@ def check_columns():
 def check_diagonals():
     # print("Checking diagonals")
     result = 0
-    if matrix[0][0] == matrix[1][1] and matrix[1][1] == matrix[2][2]:
-        result = matrix[0][0]
-    elif matrix[0][2] == matrix[1][1] and matrix[1][1] == matrix[2][0]:
-        result = matrix[0][2]
+    # if matrix[0][0] == matrix[1][1] and matrix[1][1] == matrix[2][2]:
+    #     result = matrix[0][0]
+    # elif matrix[0][2] == matrix[1][1] and matrix[1][1] == matrix[2][0]:
+    #     result = matrix[0][2]
+    for x in [0,3,6,2,5,8]:
+        if x%3==0:
+            y=x+1
+            z=x+2
+        else:
+            y=x-1
+            z=x-2
+
+        if matrix[x][0] == matrix[y][1] == matrix[z][2]:
+            result = matrix[x][0]
+            if result != 0:
+                break
+        if matrix[x][3] == matrix[y][4] == matrix[z][5]:
+            result = matrix[x][3]
+            if result != 0:
+                break
+        if matrix[x][6] == matrix[y][7] == matrix[z][8]:
+            result = matrix[x][6]
+            if result != 0:
+                break
+
     return result
 
 def check_winner():
@@ -96,7 +136,7 @@ def start_server():
     try:
         s.bind((host, port))
         print("Tic Tac Toe server started \nBinding to port", port)
-        s.listen() 
+        s.listen(2) 
         accept_players()
     except socket.error as e:
         print("Server binding error:", e)
